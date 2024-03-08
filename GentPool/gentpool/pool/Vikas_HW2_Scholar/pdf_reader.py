7023e8fefdceb29e4d772ffb5e2fa414ff7542f2
vikasai57 <50866847+vikasai57@users.noreply.github.com>
2024-03-08 15:18:42 -0500

    Add files via upload

---
+++ GentPool/gentpool/pool/Vikas_HW2_Scholar/pdf_reader.py
@@ -0,0 +1,11 @@
+import PyPDF2
+
+def read_pdf(file_path):
+    text = ""
+    with open(file_path, "rb") as file:
+        pdf_reader = PyPDF2.PdfReader(file)
+        for page_num in range(len(pdf_reader.pages)):
+            page = pdf_reader.pages[page_num]
+            text += page.extract_text()
+    return text
+
